d577b5a57ff0fd148374d33f3d0e77a060f0f551
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-24 23:23:44 -0300

    versão demo
    primeira versão concluída

--- script.js
+++ script.js
@@ -1,32 +1,83 @@
-const produtos = ['Arroz', 'Feijão', 'Açucar', 'Sal',
+
+
+let produtos = ['Arroz', 'Feijão', 'Açucar', 'Sal',
     'Óleo', 'farinha', 'Trigo', 'Macarrão',
     'Café', 'Temperos', 'Verduras', 'Frutas', 'Biscoitos',
     'Leite', 'Manteira', 'Iorgute', 'Queijo',
     'Presunto', 'Pão', 'Carnes', 'Suco',
-    'Cerveja', 'Refrigenrante', 'Chocolate', 
+    'Cerveja', 'Refrigenrante', 'Chocolate',
     'Sorvete', 'Produtos de Higienes', 'Produtos de Limpeza']
 
 
-const Resultado = document.getElementById('resultado')
-const listaEditada = document.getElementById('lista-edit')
+let lista = document.getElementById('lista')
+let resultado = document.getElementById('resultado')
 
 let selecionados = []
 
-
-const lista = document.createElement ('ul');
-
-produtos.forEach(item => {
-    const listItem = document.createElement ('li');
-    const checkbox = document.createElement('input');
+produtos.forEach(produto => {
+    const li = document.createElement('li')
+    const checkbox = document.createElement('input')
     checkbox.type = 'checkbox';
-    checkbox.id = item;
+    checkbox.id = produto;
+    const label = document.createElement('label');
+    label.textContent = produto;
 
-    listItem.textContent = item;
+    checkbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
 
-    lista.appendChild(listItem);
-    listItem.appendChild(checkbox);
-    
+            selecionados.push(produto);
+            li.classList.add('selected');
+
+            const novaLista = document.createElement('li');
+            novaLista.textContent = produto;
+            novaLista.id = 'sel-' + produto;
+            resultado.appendChild(novaLista);
+
+
+
+
+        } else {
+            selecionados = selecionados.filter(item => item !== produto);
+            li.classList.remove('selected');
+
+            const itemRemover = document.getElementById('sel-' + produto);
+            if (itemRemover) itemRemover.remove();
+        }
+
+        console.log('selecionados:', selecionados);
+
+
+
+
+    })
+
+    li.appendChild(checkbox);
+    li.appendChild(label)
+    lista.appendChild(li)
+
+
+
+
 })
+function limpar() {
 
-Resultado.appendChild(lista)
+    resultado.innerHTML = ``
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
